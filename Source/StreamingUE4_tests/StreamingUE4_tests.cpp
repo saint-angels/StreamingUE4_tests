ec50a0f44f9d20a41ffff1f5c3ae7891d22fc747
Michael <mikeprokofyev@gmail.com>
2020-05-29 12:11:53 +0300

    init
    init 2

---
+++ Source/StreamingUE4_tests/StreamingUE4_tests.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "StreamingUE4_tests.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StreamingUE4_tests, "StreamingUE4_tests" );
